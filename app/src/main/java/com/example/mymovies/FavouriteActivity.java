f94f2963dc497fb0108b058bee13a3ac992cc527
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2020-11-04 18:13:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mymovies/FavouriteActivity.java
@@ -0,0 +1,82 @@
+package com.example.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.mymovies.data.FavouriteMovie;
+import com.example.mymovies.data.MainViewModel;
+import com.example.mymovies.data.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavouriteActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewFavouriteMovies;
+    private MovieAdapter adapter;
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+        recyclerViewFavouriteMovies = findViewById(R.id.recyclerViewFavouriteMovie);
+        recyclerViewFavouriteMovies.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter = new MovieAdapter();
+        recyclerViewFavouriteMovies.setAdapter(adapter);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        LiveData<List<FavouriteMovie>> favouriteMovies = viewModel.getFavouriteMovies();
+        favouriteMovies.observe(this, new Observer<List<FavouriteMovie>>() {
+            @Override
+            public void onChanged(List<FavouriteMovie> favouriteMovies) {
+                List<Movie> movies = new ArrayList<>();
+                if (favouriteMovies != null){
+                    movies.addAll(favouriteMovies);
+                    adapter.setMovies(movies);
+                }
+            }
+        });
+        adapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Movie movie = adapter.getMovies().get(position);
+                Intent intent = new Intent(FavouriteActivity.this, DetailActivity.class);
+                intent.putExtra("id", movie.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itemMain:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.itemFavourite:
+                Intent intentToFavourite = new Intent(this, FavouriteActivity.class);
+                startActivity(intentToFavourite);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
